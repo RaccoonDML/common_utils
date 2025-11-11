f17e32a4c1ce27dda95880cb6ba1055a51cb690c
raccoondml <2684813584@qq.com>
2025-11-11 17:36:49 +0800

    add gp2linux-path

--- dmlutils/__init__.py
+++ dmlutils/__init__.py
@@ -8,8 +8,8 @@ __email__ = "2684813584@qq.com"
 
 
 # 导出核心接口
-from .file import setup_save_dir
+from .file import setup_save_dir, gp2linux_path
 from .seed import set_seed
 from .timer import timeit, change_str_color
 
-__all__ = ["setup_save_dir", "set_seed", "timeit", "change_str_color"]
+__all__ = ["setup_save_dir", "set_seed", "timeit", "change_str_color", "gp2linux_path"]
--- dmlutils/file.py
+++ dmlutils/file.py
@@ -6,3 +6,9 @@ def setup_save_dir(save_dir, clean=True):
         shutil.rmtree(save_dir)
     os.makedirs(save_dir, exist_ok=True)
     return save_dir
+
+def gp2linux_path(path):
+    ### r"\\files.miguocomics.co\内容空间\☆暗恋对象是只猫\03线稿\暗恋喵-003-线稿" => 
+    item_list = [x for x in path.split('\\') if x != '' and x!='files.miguocomics.co']
+    prefix = "/home/daimingliang/workspace/gongpan"
+    return os.path.join(prefix, *item_list)
